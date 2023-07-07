a3cfcdea724a0f9e2c88014e98bec90af5ab73df
Vlad M <vlad.milytin@gmail.com>
2023-07-08 00:36:07 +0300

    Fix package.json

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ function Game() {
       const [xIsNext, setXIsNext] = useState(true);
       const [history, setHistory] = useState([Array(9).fill(null)]);
       const [currentMove, setCurrentMove] = useState(0);
-      const currentSquares = history[currentMove];
+      const currentSquares = history[history.length - 1];
         function handlePlay(nextSquares) {
           const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
           setHistory(nextHistory);
